52303eeedfe13723028f18f83172b0fe66f15c62
Mark Washenberger <mwashenb@gmail.com>
2010-11-09 16:16:39 -0500

    make echo match cpplint standards

--- src/echo/echo-server-test.cc
+++ src/echo/echo-server-test.cc
@@ -1,10 +1,11 @@
+// Copyright 2010 <Mark Washenberger>
 #include "echo/echo-server.h"
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE EchoServerTest
 #include <boost/test/unit_test.hpp>
 
 struct F {
-  F() {};
+  F() {}
 };
 
 BOOST_FIXTURE_TEST_CASE(test, F) {
---
+++ src/echo/echo-server.h
@@ -0,0 +1,4 @@
+// Copyright 2010 <Mark Washenberger>
+#ifndef SRC_ECHO_ECHO_SERVER_H_
+#define SRC_ECHO_ECHO_SERVER_H_
+#endif  // SRC_ECHO_ECHO_SERVER_H_
